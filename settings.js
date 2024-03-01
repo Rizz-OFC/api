8a0bbaa621f4ca16a04635ca48f81977da05a79b
Rizz-OFC <161809385+Rizz-OFC@users.noreply.github.com>
2024-03-01 14:11:08 +0700

    Update settings.js
    Upload

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://rizzdev06:akurizki098@cluster0.4jvlsef.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rizzdev06@gmail.com" //email
+global.email_password = "wotg tnxl freq unvm" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
